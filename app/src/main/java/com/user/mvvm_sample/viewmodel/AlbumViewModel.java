82bb2c55bbe5823ee13a57d8952d0390be2b13b5
bob <dineshgct11@gmail.com>
2019-04-27 18:46:08 +0530

    Reterofit -Integration and basic data binding done

---
+++ app/src/main/java/com/user/mvvm_sample/viewmodel/AlbumViewModel.java
@@ -0,0 +1,51 @@
+package com.user.mvvm_sample.viewmodel;
+
+
+
+import android.arch.lifecycle.ViewModel;
+
+import com.user.mvvm_sample.interfaces.Details;
+
+import com.user.mvvm_sample.model.RetroPhoto;
+import com.user.mvvm_sample.service.ReterofitClient;
+
+import java.util.List;
+
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class AlbumViewModel extends ViewModel {
+
+    private List<RetroPhoto> retroPhotos;
+    public String bindText="tttttt";
+
+    public List<RetroPhoto> getRetroPhotos() {
+        if(retroPhotos==null) {
+            
+            retroPhotos= loadAlbums();
+        }
+        return retroPhotos;
+    }
+
+    private List<RetroPhoto> loadAlbums() {
+
+      Details service= ReterofitClient.getRetrofit().create(Details.class);
+      Call<List<RetroPhoto>> data=service.getAllPhotos();
+
+      data.enqueue(new Callback<List<RetroPhoto>>() {
+          @Override
+          public void onResponse(Call<List<RetroPhoto>> call, Response<List<RetroPhoto>> response) {
+              retroPhotos=response.body();
+          }
+
+          @Override
+          public void onFailure(Call<List<RetroPhoto>> call, Throwable t) {
+
+          }
+      });
+
+        return retroPhotos;
+    }
+}
